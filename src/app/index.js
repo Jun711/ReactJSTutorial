7ffccb70657e1b8460134ab24d3c8b15a75d5c28
Jun <cygoh@umich.edu>
2017-01-17 18:39:55 -0800

    added the first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
 console.log("It works!")
+
+class App extends React.Component {
+	// component has a lot built-in methods
+	// when reactjs thinks it needs to render a component
+	render() {
+		return( // we can only return one root element, nested elements are fine, no sibling element
+			<div>
+			   <h1>Hello!</h1>
+			</div>
+		);
+	}
+}
+
+render(<App/>, window.document.getElementById("app")); 
+// similar to selector in Angular2
+// the first argument is the object to render
+// the second argument is the place to render it
