a684926f3b0c2c38ab92733ef90780f9052cc683
Jun <cygoh@umich.edu>
2017-01-23 01:07:09 -0800

    included async call and using promise and timeout to simulate async call

--- src/app/actions/userActions.js
+++ src/app/actions/userActions.js
@@ -1,13 +1,37 @@
+// export function setName(name) {
+// 	return {
+// 		type: "SET_NAME",
+// 		payload: name
+// 	};
+// }
+
+
 export function setName(name) {
+	// return dispatch => {
+	// 	setTimeout(() => {
+	// 		dispatch({
+	// 			type: "SET_NAME",
+	// 			payload: name
+	// 		});
+	// 	}, 5000);
+	// }
+	// Promise constructor function has 2 functions as its arguments
 	return {
 		type: "SET_NAME",
-		payload: name
+		payload: new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(name);
+			}, 2000);
+		})
 	};
 }
+
+// Uncaught Error: Actions must be plain objects. Use custom middleware for async actions.
+
 
 export function setAge(age) {
 	return {
 		type: "SET_AGE", 
 		payload: age
-	}
+	};
 }
